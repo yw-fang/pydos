ab627ffdb9adac0971cf022f967b2efd7da5d240
yw-fang <fyuewen@gmail.com>
2018-10-08 23:03:52 -0700

    pydos draft

---
+++ pydos-SOC.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+
+"""
+This script will split DOSCAR from spin-orbital coupling calculations in VASP,
+you can split DOSCAR only or even plot them in a publication quality.
+"""
+
+print("Please prepare DOSCAR, POSCAR, OUTCAR from scf calculations")
